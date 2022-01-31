165c1bebdf7f949957674f060198ecc351a8f1d2
Daniel Fernando Lourusso <daniel.lourusso@db1.com.br>
2022-01-31 16:18:04 -0300

    feat: adiciona exemplo de Overlay

--- src/Maps/index.tsx
+++ src/Maps/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {View, StyleSheet, Dimensions, Alert} from 'react-native'
-import MapView, {Polygon} from 'react-native-maps';
+import MapView, {Polygon, Overlay} from 'react-native-maps';
 import polygons from "./polygons";
 
 const styles = StyleSheet.create({
@@ -33,6 +33,8 @@ export const Maps: React.FC = () => {
                }}
                showsMyLocationButton
       >
+        <Overlay bounds={[[-23.4126637384518, -51.936398200968554], [-23.414130709359927, -51.933136634806445]]}
+        image={require('../../assets/loteamento.jpeg')}/>
         {
           polygons.map(item => <Polygon key={item.id} coordinates={item.coordinates}
                                         tappable
--- src/Maps/polygons.ts
+++ src/Maps/polygons.ts
@@ -1,38 +1,40 @@
+// https://developers.google.com/maps/documentation/javascript/examples/drawing-tools
+// http://jsfiddle.net/rvsMH/1/
 const polygons = [
   {
     id: '1',
     coordinates: [
-      {latitude: -23.4126637384518, longitude: -51.936398200968554},
-      {latitude: -23.412644047456514, longitude: -51.93474596021538},
-      {latitude: -23.413313539652528, longitude: -51.93474596021538},
-      {latitude: -23.413313539652528, longitude: -51.936355285624316}
+      {latitude: -23.41313287164877, longitude: -51.93468575247862},
+      {latitude: -23.413354394358485, longitude: -51.93446044692137},
+      {latitude: -23.41323132623221, longitude: -51.93429951438048},
+      {latitude: -23.41302949425732, longitude: -51.93452481993773}
     ]
   },
   {
     id: '2',
     coordinates: [
-      {latitude: -23.413461221298558, longitude: -51.93479960439568},
-      {latitude: -23.414150400133988, longitude: -51.93478887555962},
-      {latitude: -23.414140554747327, longitude: -51.93644111631279},
-      {latitude: -23.413421839542405, longitude: -51.936398200968554}
+      {latitude: -23.41300488058078, longitude: -51.93448726901152},
+      {latitude: -23.41294088500036, longitude: -51.93429951438048},
+      {latitude: -23.413177176220373, longitude: -51.934101030913375},
+      {latitude: -23.413236248959446, longitude: -51.93428342112639}
     ]
   },
   {
     id: '3',
     coordinates: [
-      {latitude: -23.413441530421956, longitude: -51.93470304487114},
-      {latitude: -23.414130709359927, longitude: -51.93469231603508},
-      {latitude: -23.414130709359927, longitude: -51.933136634806445},
-      {latitude: -23.41352029391083, longitude: -51.933136634806445}
+      {latitude: -23.412975344162888, longitude: -51.93419222601988},
+      {latitude: -23.412916271307342, longitude: -51.93396692046263},
+      {latitude: -23.4131427171104, longitude: -51.933854267684005},
+      {latitude: -23.41318209894963, longitude: -51.93406347998717}
     ]
   },
   {
     id: '4',
     coordinates: [
-      {latitude: -23.413323385100725, longitude: -51.93467085836296},
-      {latitude: -23.412673583948344, longitude: -51.93468158719902},
-      {latitude: -23.412614510958083, longitude: -51.933104448298266},
-      {latitude: -23.413402148659927, longitude: -51.933125905970385}
+      {latitude: -23.412916271307342, longitude: -51.9339079118643},
+      {latitude: -23.41288181212942, longitude: -51.93372552165129},
+      {latitude: -23.41309841252726, longitude: -51.933607504454635},
+      {latitude: -23.41313287164877, longitude: -51.933816716757796}
     ]
   }
 ]
