2d5f38ced8c01ab1bf7b47f90e6493a45daa62ce
Daniel Fernando Lourusso <daniel.lourusso@db1.com.br>
2022-01-31 15:47:36 -0300

    feat: adiciona exemplo com polígonos

--- src/Maps/index.tsx
+++ src/Maps/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import {View, StyleSheet, Dimensions} from 'react-native'
-import MapView from 'react-native-maps';
+import {View, StyleSheet, Dimensions, Alert} from 'react-native'
+import MapView, {Polygon} from 'react-native-maps';
+import polygons from "./polygons";
 
 const styles = StyleSheet.create({
   container: {
@@ -16,18 +17,30 @@ const styles = StyleSheet.create({
 });
 
 export const Maps: React.FC = () => {
+  const onPressItem = (itemId: string) => {
+    Alert.alert('Polygon pressed', itemId)
+  }
+
   return (
     <View style={styles.container}>
       <MapView style={styles.map}
                provider='google'
                initialRegion={{
-                 latitude: -23.410897571197015,
-                 longitude: -51.93811567002402,
-                 latitudeDelta: 0.08,
-                 longitudeDelta: 0.08
+                 latitude: -23.412897571197015,
+                 longitude: -51.93511567002402,
+                 latitudeDelta: 0.008,
+                 longitudeDelta: 0.008
                }}
                showsMyLocationButton
-      />
+      >
+        {
+          polygons.map(item => <Polygon key={item.id} coordinates={item.coordinates}
+                                        tappable
+                                        onPress={() => onPressItem(item.id)}/>)
+        }
+
+      </MapView>
+
     </View>
   );
 };
---
+++ src/Maps/polygons.ts
@@ -0,0 +1,40 @@
+const polygons = [
+  {
+    id: '1',
+    coordinates: [
+      {latitude: -23.4126637384518, longitude: -51.936398200968554},
+      {latitude: -23.412644047456514, longitude: -51.93474596021538},
+      {latitude: -23.413313539652528, longitude: -51.93474596021538},
+      {latitude: -23.413313539652528, longitude: -51.936355285624316}
+    ]
+  },
+  {
+    id: '2',
+    coordinates: [
+      {latitude: -23.413461221298558, longitude: -51.93479960439568},
+      {latitude: -23.414150400133988, longitude: -51.93478887555962},
+      {latitude: -23.414140554747327, longitude: -51.93644111631279},
+      {latitude: -23.413421839542405, longitude: -51.936398200968554}
+    ]
+  },
+  {
+    id: '3',
+    coordinates: [
+      {latitude: -23.413441530421956, longitude: -51.93470304487114},
+      {latitude: -23.414130709359927, longitude: -51.93469231603508},
+      {latitude: -23.414130709359927, longitude: -51.933136634806445},
+      {latitude: -23.41352029391083, longitude: -51.933136634806445}
+    ]
+  },
+  {
+    id: '4',
+    coordinates: [
+      {latitude: -23.413323385100725, longitude: -51.93467085836296},
+      {latitude: -23.412673583948344, longitude: -51.93468158719902},
+      {latitude: -23.412614510958083, longitude: -51.933104448298266},
+      {latitude: -23.413402148659927, longitude: -51.933125905970385}
+    ]
+  }
+]
+
+export default polygons
