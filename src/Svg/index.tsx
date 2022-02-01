9aafde3debf85018bb4e6f756eb68428e0cbcc6f
Daniel Fernando Lourusso <daniel.lourusso@db1.com.br>
2022-02-01 15:02:33 -0300

    feat: ajusta viewbox do svg

--- src/Svg/index.tsx
+++ src/Svg/index.tsx
@@ -3,7 +3,9 @@ import {Alert, Dimensions, ImageBackground, StyleSheet} from 'react-native'
 import RnSvg, {Polygon} from 'react-native-svg';
 import polygons from "./polygons";
 
-const {width, height} = Dimensions.get('window')
+// const {width, height} = Dimensions.get('window')
+const {width, height} = {width: 375, height: 667}
+
 const styles = StyleSheet.create({
   container: {
     width,
@@ -17,13 +19,13 @@ export const Svg: React.FC = (props) => {
 
   return (
     <ImageBackground style={styles.container} source={require('../../assets/loteamento.jpeg')}>
-      <RnSvg height="100%" width="100%" viewBox="0 0 100 100"   {...props}>
+      <RnSvg height={height} width={width} viewBox={`0 0 ${width} ${height}`}   {...props}>
         {polygons.map(item => <Polygon
           onPress={() => onPressItem(item.id)}
           key={item.id}
           points={item.points}
-          fill="lime"
-          stroke="purple"
+          fill="rgba(255, 0, 0, 0.2)"
+          stroke="lime"
           strokeWidth="1"
         />)}
       </RnSvg>
