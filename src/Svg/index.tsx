0fdd22f2967f6757a402bf2b19c0779b92202b6e
Daniel Fernando Lourusso <daniel.lourusso@db1.com.br>
2022-02-01 15:12:59 -0300

    feat: adiciona exemplo de zoom no svg

--- src/Svg/index.tsx
+++ src/Svg/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Alert, Dimensions, ImageBackground, StyleSheet} from 'react-native'
 import RnSvg, {Polygon} from 'react-native-svg';
 import polygons from "./polygons";
+import PinchZoomView from "react-native-pinch-zoom-view";
 
 // const {width, height} = Dimensions.get('window')
 const {width, height} = {width: 375, height: 667}
@@ -18,17 +19,23 @@ export const Svg: React.FC = (props) => {
   }
 
   return (
-    <ImageBackground style={styles.container} source={require('../../assets/loteamento.jpeg')}>
-      <RnSvg height={height} width={width} viewBox={`0 0 ${width} ${height}`}   {...props}>
-        {polygons.map(item => <Polygon
-          onPress={() => onPressItem(item.id)}
-          key={item.id}
-          points={item.points}
-          fill="rgba(255, 0, 0, 0.2)"
-          stroke="lime"
-          strokeWidth="1"
-        />)}
-      </RnSvg>
-    </ImageBackground>
+    <PinchZoomView maxScale={5}>
+      <ImageBackground style={styles.container}
+        // resizeMode='contain'
+                       source={require('../../assets/loteamento.jpeg')}
+      >
+        <RnSvg height={height} width={width} viewBox={`0 0 ${width} ${height}`}   {...props}>
+          {polygons.map(item => <Polygon
+            onPress={() => onPressItem(item.id)}
+            key={item.id}
+            points={item.points}
+            fill="rgba(255, 0, 0, 0.2)"
+            stroke="lime"
+            strokeWidth="1"
+          />)}
+        </RnSvg>
+      </ImageBackground>
+    </PinchZoomView>
+
   );
 };
